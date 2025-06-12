53d5d33707d1a48c6e78b1351fe752416cb1810d
Steven Suing <Steven.suing@ais.com>
2025-06-12 17:51:34 -0400

    fix for stuff

--- src/Message/Program.cs
+++ src/Message/Program.cs
@@ -11,34 +11,17 @@ using System.Collections;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-using var loggerFactory = LoggerFactory.Create(logging =>
-{
-    logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
-    logging.AddConsole();
-});
-var startupLogger = loggerFactory.CreateLogger("Startup");
-
-
-startupLogger.LogInformation($"DOTNET_ENVIRONMENT = {Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")}");
-
-// Log the value of AzureServiceBus from each configuration provider
-var configRoot = (IConfigurationRoot)builder.Configuration;
-foreach (var provider in configRoot.Providers)
-{
-    if (provider.TryGet("ConnectionStrings:AzureServiceBus", out var value))
-    {
-        startupLogger.LogInformation($"Provider {provider} supplies ConnectionStrings:AzureServiceBus = '{value}'");
-    }
-    else
-    {
-        startupLogger.LogInformation($"Provider {provider} does NOT supply ConnectionStrings:AzureServiceBus");
-    }
-}
-
-if (builder.Environment.IsDevelopment())
-{
-
-    var epc = NServiceBusConfigurator.DevelopmentConfiguration(
+// NServiceBus endpoint configuration
+var endpointConfig = builder.Environment.IsDevelopment()
+    ? NServiceBusConfigurator.DevelopmentConfiguration(
+        builder.Configuration,
+        "EventManagement.Api",
+        routingSettings =>
+        {
+            // Configure routing settings here if needed
+            // Example: routingSettings.RouteToEndpoint(typeof(MyCommand), "MyDestinationEndpoint");
+        })
+    : NServiceBusConfigurator.ProductionConfiguration(
         builder.Configuration,
         "EventManagement.Api",
         routingSettings =>
@@ -46,28 +29,9 @@ if (builder.Environment.IsDevelopment())
             // Configure routing settings here if needed
             // Example: routingSettings.RouteToEndpoint(typeof(MyCommand), "MyDestinationEndpoint");
         });
-    var endpointWithExternallyManagedContainer = EndpointWithExternallyManagedContainer
-        .Create(epc, builder.Services);
 
-    builder.Services.AddSingleton(p => endpointWithExternallyManagedContainer.MessageSession.Value);
-    var endpoint = await endpointWithExternallyManagedContainer.Start(builder.Services.BuildServiceProvider());
-}
-else
-{
-    var epc = NServiceBusConfigurator.ProductionConfiguration(
-        builder.Configuration,
-        "EventManagement.Api",
-        routingSettings =>
-        {
-            // Configure routing settings here if needed
-            // Example: routingSettings.RouteToEndpoint(typeof(MyCommand), "MyDestinationEndpoint");
-        });
-    var endpointWithExternallyManagedContainer = EndpointWithExternallyManagedContainer
-        .Create(epc, builder.Services);
-
-    builder.Services.AddSingleton(p => endpointWithExternallyManagedContainer.MessageSession.Value);
-    var endpoint = await endpointWithExternallyManagedContainer.Start(builder.Services.BuildServiceProvider());
-}
+// Register NServiceBus as a hosted service
+builder.UseNServiceBus(endpointConfig);
 
 // Register infrastructure and NServiceBus BEFORE builder.Build()
 DependencyInjection.AddInfrastructure(builder.Services, builder.Configuration, builder.Environment.IsDevelopment());
